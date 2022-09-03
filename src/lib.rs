83e04845971013b36af55236ae06284ace9e2136
Todd Everett Griffin <toddgriffin@protonmail.com>
2022-09-02 17:55:36 -0700

    Clean up lib.rs rustdoc. Exclude .idea/ from 'cargo publish'.

--- src/lib.rs
+++ src/lib.rs
@@ -10,26 +10,24 @@
 //! use sitemap_rs::url_set::UrlSet;
 //! use std::path::PathBuf;
 //!
-//! fn main() {
-//!     let urls: Vec<Url> = vec![Url::new(
-//!         String::from("http://www.example.com/"),
-//!         Some(DateTime::from_utc(
-//!             NaiveDate::from_ymd(2005, 1, 1).and_hms(0, 0, 0),
-//!             FixedOffset::east(0),
-//!         )),
-//!         Some(ChangeFrequency::Monthly),
-//!         Some(0.8),
-//!         None,
-//!         None,
-//!         None,
-//!     )
-//!     .expect("failed a <url> validation")];
+//! let urls: Vec<Url> = vec![Url::new(
+//!     String::from("http://www.example.com/"),
+//!     Some(DateTime::from_utc(
+//!         NaiveDate::from_ymd(2005, 1, 1).and_hms(0, 0, 0),
+//!         FixedOffset::east(0),
+//!     )),
+//!     Some(ChangeFrequency::Monthly),
+//!     Some(0.8),
+//!     None,
+//!     None,
+//!     None,
+//! )
+//! .expect("failed a <url> validation")];
 //!
-//!     let url_set: UrlSet = UrlSet::new(urls).expect("failed a <urlset> validation");
-//!     url_set
-//!         .write_to_file(PathBuf::from("./target/url-sitemap.xml"))
-//!         .unwrap();
-//! }
+//! let url_set: UrlSet = UrlSet::new(urls).expect("failed a <urlset> validation");
+//! url_set
+//!     .write_to_file(PathBuf::from("./target/url-sitemap.xml"))
+//!     .unwrap();
 //! ```
 //!
 //! Generated XML:
