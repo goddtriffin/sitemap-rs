e0fa9c2bc338c9399a8daae79a6156f5062926cb
Todd Everett Griffin <toddgriffin@protonmail.com>
2022-09-02 17:51:41 -0700

    Create library level rustdoc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,58 @@
+//! Library to generate URL, Index, Image, Video, and News sitemaps.
+//!
+//! ## Example
+//!
+//! `cargo run --example generate_url_sitemap`
+//!
+//! ```rust
+//! use chrono::{DateTime, FixedOffset, NaiveDate};
+//! use sitemap_rs::url::{ChangeFrequency, Url};
+//! use sitemap_rs::url_set::UrlSet;
+//! use std::path::PathBuf;
+//!
+//! fn main() {
+//!     let urls: Vec<Url> = vec![Url::new(
+//!         String::from("http://www.example.com/"),
+//!         Some(DateTime::from_utc(
+//!             NaiveDate::from_ymd(2005, 1, 1).and_hms(0, 0, 0),
+//!             FixedOffset::east(0),
+//!         )),
+//!         Some(ChangeFrequency::Monthly),
+//!         Some(0.8),
+//!         None,
+//!         None,
+//!         None,
+//!     )
+//!     .expect("failed a <url> validation")];
+//!
+//!     let url_set: UrlSet = UrlSet::new(urls).expect("failed a <urlset> validation");
+//!     url_set
+//!         .write_to_file(PathBuf::from("./target/url-sitemap.xml"))
+//!         .unwrap();
+//! }
+//! ```
+//!
+//! Generated XML:
+//! ```xml
+//! <?xml version="1.0" encoding="UTF-8"?>
+//! <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+//!   <url>
+//!     <loc>http://www.example.com/</loc>
+//!     <lastmod>2005-01-01T09:10:11+00:00</lastmod>
+//!     <changefreq>monthly</changefreq>
+//!     <priority>0.8</priority>
+//!   </url>
+//! </urlset>
+//! ```
+//!
+//! ### More examples:
+//!
+//! [Generate URL sitemap](url_set/index.html#examples).
+//! [Generate Index sitemap](sitemap_index/index.html#examples).
+//! [Generate Image sitemap](image/index.html#examples).
+//! [Generate Video sitemap](video/index.html#examples).
+//! [Generate News sitemap](news/index.html#examples).
+
 use chrono::SecondsFormat;
 
 pub mod image;
