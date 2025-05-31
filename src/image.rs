99a0753bbf907d0d38e07a1153a144d39f12b84c
Finn Bear <finntbear@gmail.com>
2025-05-31 19:06:52 +0000

    Additional image schema fields.

--- src/image.rs
+++ src/image.rs
@@ -10,12 +10,44 @@ pub struct Image {
     /// If, for example, you use a content delivery network such as Google Sites to host your images, make sure that the hosting site is verified in Search Console.
     /// In addition, make sure that your robots.txt file doesn't disallow the crawling of any content you want indexed.
     pub location: String,
+    /// The caption of the image.
+    pub caption: Option<String>,
+    /// The geographical location of the image, e.g. "Limerick, Ireland".
+    pub geo_location: Option<String>,
+    /// The title of the image.
+    pub title: Option<String>,
+    /// A URL to the license of the image.
+    pub license: Option<String>,
 }
 
 impl Image {
     #[must_use]
-    pub const fn new(location: String) -> Self {
-        Self { location }
+    pub fn new(location: String) -> Self {
+        Self {
+            location,
+            caption: None,
+            geo_location: None,
+            title: None,
+            license: None,
+        }
+    }
+
+    /// Builder methods for optional fields
+    pub fn caption(mut self, caption: impl Into<String>) -> Self {
+        self.caption = Some(caption.into());
+        self
+    }
+    pub fn geo_location(mut self, geo_location: impl Into<String>) -> Self {
+        self.geo_location = Some(geo_location.into());
+        self
+    }
+    pub fn title(mut self, title: impl Into<String>) -> Self {
+        self.title = Some(title.into());
+        self
+    }
+    pub fn license(mut self, license: impl Into<String>) -> Self {
+        self.license = Some(license.into());
+        self
     }
 
     /// # Errors
@@ -28,6 +60,27 @@ impl Image {
         let mut loc: XMLElement = XMLElement::new("image:loc");
         loc.add_text(self.location)?;
         image.add_child(loc)?;
+
+        if let Some(caption) = self.caption {
+            let mut caption_el = XMLElement::new("image:caption");
+            caption_el.add_text(caption)?;
+            image.add_child(caption_el)?;
+        }
+        if let Some(geo_location) = self.geo_location {
+            let mut geo_el = XMLElement::new("image:geo_location");
+            geo_el.add_text(geo_location)?;
+            image.add_child(geo_el)?;
+        }
+        if let Some(title) = self.title {
+            let mut title_el = XMLElement::new("image:title");
+            title_el.add_text(title)?;
+            image.add_child(title_el)?;
+        }
+        if let Some(license) = self.license {
+            let mut license_el = XMLElement::new("image:license");
+            license_el.add_text(license)?;
+            image.add_child(license_el)?;
+        }
 
         Ok(image)
     }
--- tests/image.rs
+++ tests/image.rs
@@ -7,3 +7,16 @@ fn test_constructor_only_required_fields() {
     ));
     assert!(!image.location.is_empty());
 }
+
+#[test]
+fn test_all_fields() {
+    let image = Image::new("https://example.com/image.jpg".to_string())
+        .caption("A beautiful view")
+        .geo_location("New York, USA")
+        .title("Skyline")
+        .license("https://example.com/license");
+    assert_eq!(image.caption.as_deref(), Some("A beautiful view"));
+    assert_eq!(image.geo_location.as_deref(), Some("New York, USA"));
+    assert_eq!(image.title.as_deref(), Some("Skyline"));
+    assert_eq!(image.license.as_deref(), Some("https://example.com/license"));
+}
